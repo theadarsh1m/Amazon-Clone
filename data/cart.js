9481b82dd8d3a22cddcc4e863c31f5abb1bb6724
Adarsh Sachan <2k23.cs2312635@gmail.com>
2025-04-27 12:23:35 +0530

    delete poduct form cart when click delete

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,13 @@
-export const cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+export let cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1,
-}];
+  },
+];
 
 export function addToCart(productId) {
   let matchingItem = cart.find((cartItem) => cartItem.productId === productId);
@@ -16,4 +19,15 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
 }
